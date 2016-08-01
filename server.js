bdb38482100474f8ec2ec641df038b76c880f27c
brentzucker <brentzucker@gmail.com>
2016-08-01 00:43:45 -0400

    added standings code from espn into server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+'use strict';
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
+var iconvlite = require('iconv-lite');
+var cheerio = require("cheerio");
 
 //=========================================================
 // Bot Setup
@@ -57,8 +61,72 @@ bot.dialog('/scores', function (session) {
 });
 
 bot.dialog('/standings', function (session) {
-	session.send("Here are the standings");
-	session.endDialog();
+	// session.send("Here are the standings");
+	var headers = {
+	    'Accept-Language': 'en-US,en;q=0.8',
+	    'Upgrade-Insecure-Requests': '1',
+	    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	    'Referer': 'http://games.espn.go.com/flb/leagueoffice?leagueId=182799&seasonId=2016',
+	    'Connection': 'keep-alive',
+		'Cookie': process.env.COOKIE
+	};
+
+	var options = {
+	    url: 'http://games.espn.go.com/flb/standings?leagueId=182799&seasonId=2016',
+	    headers: headers,
+	    encoding: null
+	};
+
+	request(options, function(error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	    	var encoding = 'ISO-8859-1';
+       		var content = iconvlite.decode(body, encoding);
+	        // console.log(content);
+
+	        // Parse the HTML 
+			let $ = cheerio.load(content);
+
+			var standings_list = [];
+			$('tr[class=tableBody]').each(function(i, elemI) {
+				var entry = [];
+				$(this).children().each(function(j, elemJ) {
+					console.log($(this).text());
+					entry.push($(this).text());
+				})
+				standings_list.push(entry);
+			});
+
+			// calculate longest team name
+			var max = -1;
+			for (var i = 0; i < standings_list.length; i++) {
+				if (standings_list[i][0].length > max) {
+					max = standings_list[i][0].length;
+				}
+			}
+
+			// add padding
+			max += 5;
+
+			// Generate standings string
+			var standings = [];
+			for (var i = 0; i < standings_list.length; i++) {
+				var s = standings_list[i];
+
+				var rank = (i+1) + '.';
+				var teamName = s[0];
+				var record = s[1] + '-' + s[2] + '-' + s[3];
+				var winPercentage = s[4];
+				
+				var str = rank + ' ' + teamName + ' '.repeat(max - teamName.length) + record + ' ' + winPercentage;
+
+				standings.push(str);
+			}
+
+	        session.send(standings);
+	        session.endDialog();
+	    }
+	});
 });
 
 bot.dialog('/transactions', function (session) {
